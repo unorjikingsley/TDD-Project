20b4e0815ea96c12b7760e6689e2a15143c73d35
unorji kingsley <unorjikingsley@gmail.com>
2023-08-04 18:54:03 +0100

    more explanation

--- solver.rb
+++ solver.rb
@@ -9,6 +9,21 @@ class Solver
     result
   end
 
+  # def factorial(integer)
+  #   raise 'exception error' if integer.negative?
+
+  #   factorial = 1
+  #   integer.times do
+  #     factorial *= integer
+  #     integer -= 1
+  #   end
+  #   factorial
+  # end
+
+  # def reverse(string)
+  #   string.reverse
+  # end
+
   def reverse(word)
     word.reverse
   end
@@ -20,5 +35,19 @@ class Solver
 
     # Any other case, return N as a string (e.g. say N is 7 then return "7")
     num.to_s
+
+    # def fizzbuzz(integer)
+    # if (integer % 3).zero? && (integer % 5).zero?
+    #   'fizzbuzz'
+
+    # elsif (integer % 3).zero?
+    #   'fizz'
+
+    # elsif (integer % 5).zero?
+    #   'buzz'
+
+    # else
+    #   integer.to_s
+    end
   end
 end
